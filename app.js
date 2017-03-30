6e23a7c813893a17df9b012aac52cb9d33dd7fa9
Chris Wilczewski <c.wilczewski@gmail.com>
2017-03-29 21:12:41 -0400

    Added the ability for the app to download posters and background images and store them in specific folders, as well as added an API call for cast and director -Chris

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var http = require('http')
 var app = express();
 var socketio = require('socket.io'); //potentially not needed
 var path = require('path');
@@ -30,7 +31,8 @@ app.use(upload.array()); // for parsing multipart/form-data
 MongoClient.connect(url, function (err, db) {
     if (err) {
         console.log('Unable to connect to the mongoDB server. Error:', err);
-    } else {
+    }
+    else {
         console.log('Connection established to', url);
         app.locals.db = db;
         //start syncing information
@@ -45,39 +47,84 @@ MongoClient.connect(url, function (err, db) {
                     db.collection('movies', function (err, collection) {
                         collection.findOne({
                             real_name: {
-                                $exists: true,
-                                $eq: movieName
+                                $exists: true
+                                , $eq: movieName
                             }
                         }, function (err, doc) {
                             if (doc) {
                                 //if names are present in collection then skip
                                 console.log("--SKIPPING: " + actualName + " is already in the the DB!");
-
-                            } else {
+                            }
+                            else {
                                 //if names are not present in collection then search the api and add to the database
                                 console.log("--ADDING: " + actualName + " to the DB!");
+                                //search the API for the movie name
                                 getJSON("https://api.themoviedb.org/3/search/movie?api_key=9a094988e96e2398c36e5d3b8727b1c0&language=en-US&query=" + movieName, function (error, data) {
                                     if (data.results[0] != undefined) {
                                         data.results[0].filepath = "directory/" + f + "";
-                                        // Each time we loop through the array, we will add the movie to your Movie array
-                                        db.collection('movies', function (err, collection) {
-                                            collection.insert({
-                                                //inserting information to database
-                                                _id: data.results[0].title,
-                                                id: data.results[0].id,
-                                                title: data.results[0].title,
-                                                real_name: movieName,
-                                                release_date: data.results[0].release_date,
-                                                poster_path: data.results[0].poster_path,
-                                                backdrop_path: data.results[0].backdrop_path,
-                                                genre_ids: data.results[0].genre_ids[0],
+                                        //save the movie id to search for cast information
+                                        var id = data.results[0].id;
+                                        //save the poster image for each film to local folder
+                                        posterOptions = {
+                                            host: "image.tmdb.org"
+                                            , port: 80
+                                            , path: "/t/p/w500" + data.results[0].poster_path
+                                        }
+                                        var request = http.get(posterOptions, function (res) {
+                                            var imagedata = ''
+                                            res.setEncoding('binary')
+                                            res.on('data', function (chunk) {
+                                                imagedata += chunk
+                                            });
+                                            res.on('end', function () {
+                                                fs.writeFile("client/images/posters/" + movieName + '.jpg', imagedata, 'binary', function (err) {
+                                                    if (err) throw err
+                                                    console.log('Poster image saved')
+                                                });
+                                            });
+                                        });
+                                        //save backdrop image for each file to local folder
+                                        backdropOptions = {
+                                            host: "image.tmdb.org"
+                                            , port: 80
+                                            , path: "/t/p/w500" + data.results[0].backdrop_path
+                                        }
+                                        var request = http.get(backdropOptions, function (res) {
+                                            var imagedata = ''
+                                            res.setEncoding('binary')
+                                            res.on('data', function (chunk) {
+                                                imagedata += chunk
+                                            });
+                                            res.on('end', function () {
+                                                fs.writeFile("client/images/backdrops/" + movieName + '.jpg', imagedata, 'binary', function (err) {
+                                                    if (err) throw err
+                                                    console.log('Backdrop image saved')
+                                                });
+                                            });
+                                        });
+                                        //get the cast information from the API
+                                        getJSON("https://api.themoviedb.org/3/movie/" + id + "/credits?api_key=9a094988e96e2398c36e5d3b8727b1c0", function (error, cData) {
+                                            //Each time we loop through the array, we will add the movie to your Movie array
+                                            db.collection('movies', function (err, collection) {
+                                                collection.insert({
+                                                    //inserting information to database
+                                                    _id: data.results[0].id
+                                                    , title: data.results[0].title
+                                                    , real_name: movieName
+                                                    , release_date: data.results[0].release_date
+                                                    , poster_path: data.results[0].poster_path
+                                                    , backdrop_path: data.results[0].backdrop_path
+                                                    , genre_ids: data.results[0].genre_ids[0]
+                                                    , cast: [cData.cast[0].name, cData.cast[1].name, cData.cast[2].name]
+                                                    , director: cData.crew[0].name
+                                                });
                                             });
                                         });
                                     }
                                 });
                             }
-                        })
-                    })
+                        });
+                    });
                 });
             });
         });
